6241c8c532704b8e113312a2191c1b979f8bf2c0
Pierre Ficheux <pierre.ficheux@smile.fr>
2021-10-09 08:55:42 +0200

    Move/rename set_leds.sh

--- Dragon_PCI-L/PCI/driver/char/set_leds_pi.sh
+++ Dragon_PCI-L/PCI/driver/char/set_leds_pi.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-set -x
-
+# set leds and send a picture from Pi
+ 
 RPI=192.168.1.19
 
 dragon_pci_test /dev/dragon_pci_mem0 1 $1 0
-#ssh  root@$RPI /home/root/camera.sh; scp root@$RPI:~/dragon_pci.png ~pierre/public_html
 ssh  root@$RPI raspistill -w 640 -h 480 -e png -o dragon_pci.png; scp root@$RPI:~/dragon_pci.png ~pierre/public_html
 
